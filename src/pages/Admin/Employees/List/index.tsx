679a898caf5546d891bb792c9cccee3fbaddb687
Adriano Marques da Silva <kameloth2004@hotmail.com>
2023-05-26 08:50:47 -0300

    EmployeeCard render

--- src/pages/Admin/Employees/List/index.tsx
+++ src/pages/Admin/Employees/List/index.tsx
@@ -1,20 +1,39 @@
-import './styles.css';
-
 import Pagination from 'components/Pagination';
 import EmployeeCard from 'components/EmployeeCard';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Employee } from 'types/employee';
+import { AxiosRequestConfig } from 'axios';
+import { BASE_URL, requestBackend } from 'util/requests';
 
-const employeeHardCode = { // delete
-  id: 1,
-  name: "Carlos",
-  email: "carlos@gmail.com",
-  department: {
-    id: 1,
-    name: "Sales"
-  }
-};
+import './styles.css';
+
 
 const List = () => {
+
+  const [page, setPage] = useState<SpringPage<Employee>>();
+
+  useEffect(() => {
+    const params: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/employees/',
+      baseURL: BASE_URL,
+      params: {
+        page: 0,
+        size: 4
+      },
+      withCredentials: true,
+
+    };
+
+    requestBackend(params)
+      .then(response => {
+        setPage(response.data);
+        console.log(page);
+      });
+
+  }, []);
 
   const handlePageChange = (pageNumber: number) => {
     // to do
@@ -28,10 +47,12 @@ const List = () => {
         </button>
       </Link>
 
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
+      {page?.content.map(employee => {
+        return (
+          <EmployeeCard employee={employee} />
+        );
+      })}
+
 
       <Pagination
         forcePage={0}
